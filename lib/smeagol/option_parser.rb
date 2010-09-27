5867ff037e0d70ac856693da2b5076f09edaac8c
Ben Johnson <benbjohnson@yahoo.com>
2010-09-27 11:02:11 -0600

    Added configuration file support.

--- lib/smeagol/option_parser.rb
+++ lib/smeagol/option_parser.rb
@@ -2,35 +2,38 @@ module Smeagol
   class OptionParser
     # Parses the command line options.
     #
+    # args - The arguments to parse. Typically use `ARGV`.
+    #
     # Returns an OpenStruct of the options.
     def self.parse(args)
-      # Set default options
-      options = OpenStruct.new
-      options.port = 4567
-      options.auto_update = false
-      options.cache_enabled = true
-      
       # Set parse options
-      opts = ::OptionParser.new do |opts|
-        opts.banner = 'usage: smeagol [OPTIONS] [PATH]\n\n'
+      options = {}
+      parser = ::OptionParser.new do |parser|
+        parser.banner = 'usage: smeagol [OPTIONS] [PATH]\n\n'
 
-        opts.on('--port [PORT]', 'Bind port (default 4567).') do |port|
-          options.port = port.to_i
+        parser.on('--port [PORT]', 'Bind port (default 4567).') do |port|
+          options['port'] = port.to_i
         end
 
-        opts.on('--git [GIT]', 'Path to Git binary.') do |path|
-          options.git = path
+        parser.on('--git [GIT]', 'Path to Git binary.') do |path|
+          options['git'] = path
         end
 
-        opts.on('--autoupdate', 'Updates the repository on a daily basis.') do |flag|
-          options.auto_update = flag
+        parser.on('-c', '--config [PATH]', 'Loads the config file. (default /etc/smeagol/config.yml)') do |path|
+          puts "Cannot find configuration file: #{path}" unless File.exists?(path)
+          puts "Cannot read configuration file: #{path}" unless File.readable?(path)
+          options['config_path'] = path
+        end
+        
+        parser.on('--autoupdate', 'Updates the repository on a daily basis.') do |flag|
+          options['auto_update'] = flag
         end
 
-        opts.on('--[no-]cache', 'Enables page caching.') do |flag|
-          options.cache_enabled = flag
+        parser.on('--[no-]cache', 'Enables page caching.') do |flag|
+          options['cache_enabled'] = flag
         end
 
-        opts.on('-v', '--version', 'Display current version.') do
+        parser.on('-v', '--version', 'Display current version.') do
           puts "Smeagol #{Smeagol::VERSION}"
           exit 0
         end
@@ -38,14 +41,69 @@ module Smeagol
 
       # Read command line options into `options` hash
       begin
-        opts.parse!
+        parser.parse!
       rescue ::OptionParser::InvalidOption
         puts "smeagol: #{$!.message}"
         puts "smeagol: try 'smeagol --help' for more information"
         exit
       end
 
+      # Load config
+      config_path = options['config_path'] || '/etc/smeagol/config.yml'
+      config = load_config(config_path)
+      opts = default_options.merge(config).merge(options)
+      opts = opts.to_ostruct()
+
       # Attempt to find the git binary
+      update_git_path(opts)
+
+      # Set the path to the Gollum repository if not already set
+      opts.gollum_path ||= args[0] || Dir.pwd
+      
+      # Merge all options
+      return opts
+    end
+
+
+    ###########################################################################
+    #
+    # Private Methods
+    #
+    ###########################################################################
+    
+    private
+
+    # The default options for Smeagol.
+    def self.default_options
+      options = Hash.new
+      options['port'] = 4567
+      options['auto_update'] = false
+      options['cache_enabled'] = true
+      options
+    end
+    
+    # Loads a configuration file.
+    #
+    # Returns a hash of options from the config file.
+    def self.load_config(path)
+      config = {}
+      
+      if File.exists?(path)
+        # Notify the user if the config file exists but is not readable.
+        if !File.readable?(path)
+          puts "Config file not readable: #{path}"
+          exit
+        end
+        
+        config = YAML.load(IO.read(path))
+        p config
+      end
+
+      return config
+    end
+
+    # Locates the git binary in common places in the file system.
+    def self.update_git_path(options)
       if options.git.nil?
         ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/opt/local/bin'].each do |path|
           file = "#{path}/git"
@@ -60,11 +118,6 @@ module Smeagol
       else
         puts "git found: #{options.git}"
       end
-
-      # Set the path to the Gollum repository
-      options.gollum_path = args[0] || Dir.pwd
-      
-      options
     end
   end
 end
