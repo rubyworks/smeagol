724152ec9f167fb77309541bfa733ec13ffd3f12
7rans <transfire@gmail.com>
2011-12-03 19:42:10 -0500

    Copy smeagol public files for static generation.

--- lib/smeagol/static.rb
+++ lib/smeagol/static.rb
@@ -30,8 +30,16 @@ module Smeagol
       end
 
       if not File.directory?(current_directory)
-        Dir::mkdir(current_directory)
+        fileutils.mkdir(current_directory) unless File.directory?(current_directory)
       end
+    end
+
+    # Copy smeagol's default public files to static site.
+    # These files are put in a separate `smeagol` directory
+    # to avoid name clashes with wiki files.
+    def build_smeagol
+      dir = File.dirname(__FILE__) + '/public/smeagol'
+      fileutils.cp_r(dir, current_directory)
     end
 
     def build_tree(tree)
@@ -54,6 +62,7 @@ module Smeagol
           directory_push(name)
         end
 
+        puts "write #{current_directory}/index.html"
         File.open("#{current_directory}/index.html", 'w') do |f|
           f.write(Mustache.render(template, Smeagol::Views::Page.new(page)))
         end
@@ -63,7 +72,10 @@ module Smeagol
         end
       else
         file_name = "#{current_directory}/#{blob.name}"
-        FileUtils.mkdir_p(File.dirname(file_name))
+        dir_name  = File.dirname(file_name)
+        fileutils.mkdir_p(dir_name) unless File.directory?(dir_name)
+
+        puts "write #{file_name}"
         File.open(file_name, 'w') do |f|
           f.write(blob.data)
         end
@@ -71,9 +83,31 @@ module Smeagol
     end
 
     def build(directory)
+      puts "Building #{directory} ..."
+
       directory_push(directory)
+      build_smeagol
       build_tree(@wiki.repo.tree)
       directory_pop
+    end
+
+    # TODO: slug support
+    def slug(page,blob)
+      date = page.version.authored_date
+      name = blob.name[name.index(/[A-Za-z]/)..-1]
+
+      if slug = @wiki.settings.slug
+        slug = date.strftime(slug)
+        slug = slug.sub(':name', name)
+      else
+        slug = name
+      end
+      slug
+    end
+
+    #
+    def fileutils
+      FileUtils::Verbose
     end
 
   end
