79fc16c9164bcddd3a7b0e0e2e8ef0966a1b6544
trans <transfire@gmail.com>
2012-07-10 13:53:53 -0400

    Get initial demos working. [test]

--- demo/applique/web_steps.rb
+++ demo/applique/web_steps.rb
@@ -8,13 +8,13 @@ When /^I go to "([^"]*)"$/ do |url|
   get url
 end
 
-Then /^I should see the following:$/ do |body|
+When /^I should see the following:$/ do |body|
   body = body.gsub(/^\s+| +$/, '')
   response_body = last_response.body.gsub(/^\s+| +$/, '')
   response_body.should == body
 end
 
-Then /^I should see the following content:$/ do |body|
+When /^I should see the following content:$/ do |body|
   body = body.gsub(/^\s+/, '')
   response_body = get_content(last_response.body.gsub(/^\s+/, ''))
   response_body.should == body
